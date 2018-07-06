a9b8456b479746a4c923c8c310e5ef9e7cb1435d
Sunny Gupta <flameheadsg@gmail.com>
2018-07-06 00:01:42 -0700

    push to origin

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CommentBox from 'components/CommentBox';
 import CommentList from 'components/CommentList';
+import { Route, Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from 'actions';
 
-export default () => {
-  return (
-    <div>
-      <CommentBox />
-      <CommentList />
-    </div>
-  );
+class App extends Component {
+  renderButton() {
+    if (this.props.auth) {
+      return (
+        <button onClick={() => this.props.changeAuth(false)}>
+          Log Out
+        </button>
+      );
+    } else {
+      return (
+        <button onClick={() => this.props.changeAuth(true)}>
+          Log In
+        </button>
+      );
+    }
+  }
+
+  renderHeader() {
+    return (
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/post">Comment</Link>
+        </li>
+        <li>
+          {this.renderButton()}
+        </li>
+      </ul>
+    )
+  }
+
+  render() {
+    return (
+      <div>
+      {this.renderHeader()}
+        <Route path="/post" component={CommentBox} />
+        <Route exact path="/" component={CommentList} />
+      </div>
+    );
+  }
 }
+
+function mapStateToProps(state) {
+  return { auth: state.auth };
+}
+
+export default connect(mapStateToProps, actions)(App);
